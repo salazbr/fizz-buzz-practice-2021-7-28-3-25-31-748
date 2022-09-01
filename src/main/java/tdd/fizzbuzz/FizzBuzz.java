8896d7bfefc1b3fb3426b50a085527fa66b085c7
BRIAN ALLYN SALAZAR (DOCU-IRIS-ISD-OOCLL/MNL) <SALAZBR@OOCL.COM>
2022-09-01 14:14:54 +0800

    feat: implement handling for normal numbers

--- src/main/java/tdd/fizzbuzz/FizzBuzz.java
+++ src/main/java/tdd/fizzbuzz/FizzBuzz.java
@@ -1,4 +1,8 @@
 package tdd.fizzbuzz;
 
 public class FizzBuzz {
+    
+    public String countOff(String number) {
+        return number;
+    }
 }
--- src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
+++ src/test/java/tdd/fizzbuzz/FizzBuzzTest.java
@@ -1,4 +1,23 @@
 package tdd.fizzbuzz;
 
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class FizzBuzzTest {
+
+    @Test
+    void should_return_1_when_count_off_given_1(){
+        //given
+
+        FizzBuzz fizzBuzz = new FizzBuzz();
+        String expected = "1";
+
+        // when
+        String actual = "1";
+        fizzBuzz.countOff(actual);
+
+        // then
+        assertEquals(expected,actual);
+    }
 }
